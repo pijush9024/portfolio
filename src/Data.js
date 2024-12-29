338abfc7f9b1eb81c47df020320b75a18db367fc
pijush9024 <165477638+pijush9024@users.noreply.github.com>
2024-12-29 12:23:14 +0530

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -1,28 +1,22 @@
 import todo from './assets/todo.jpg';
 import placement from './assets/placement.jpg'
 import weatherapp from './assets/weather.jpg'
+import powerbi from './assets/powerbi.png'
 
 
 const Data = [
 
     {
-        image: placement,
-        title: 'Learning Courses',
-        desc: 'This project is about Placement Learning application where students can log in and buy multiple courses that we are provide. Techs: HTML, CSS, ReactJS',
-        link: 'https://github.com/pijush9024/learningcourses'
+        image: powerbi,
+        title: 'Power BI Dashboard',
+        desc: 'Over the past few weeks, I have been diving deep into Power BI, and I'm excited to present my first interactive dashboard! 📊',
+        link: '#'
     },
     {
         image: weatherapp,
         title: 'Weather Web App',
         desc: "This is a Weather Web application. Here users can search their weather situation. Techs: ReactJS, HTML, CSS",
         link: 'https://github.com/pijush9024/weatherapp'
-
-    },
-    {
-        image: todo,
-        title: 'Todo App',
-        desc: 'This is a Todo Web app. Here users can add, delete as well as mark tasks. Techs: Front end languages such as HTML, CSS, JavaScript',
-        link: 'https://github.com/pijush9024/todoapp'
 
     }
 

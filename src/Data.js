58a7293c02fe236244a293b1d6f3e7591a811866
pijush9024 <165477638+pijush9024@users.noreply.github.com>
2024-12-29 20:25:09 +0530

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -2,6 +2,7 @@ import todo from './assets/todo.jpg';
 import placement from './assets/placement.jpg'
 import weatherapp from './assets/weather.jpg'
 import powerbi from './assets/powerbi.png'
+import lookerstudi from './assets/Lookerstudio.png'
 
 
 const Data = [
@@ -12,6 +13,16 @@ const Data = [
         desc: "Over the past few weeks, I have been diving deep into Power BI, and I'm excited to present my first interactive dashboard! 📊",
         link: 'https://drive.google.com/file/d/1AHCQyFro8JKIwqinxvpO57VEdXTjxljQ/view'
     },
+
+       {
+        image: Looker Studio,
+        title: 'Looker studio Dashboard',
+        desc: "Designed a comprehensive Monthly Run Rate dashboard in Looker Studio, showcasing dynamic data visualization and analytics to track company performance across months, enhancing decision-making efficiency.! 📊",
+        link: 'https://drive.google.com/file/d/1C_VolK_PL4uVp7Hp-uap6Q4U1DVwEW7k/view?usp=drive_link'
+           
+    },
+
+   
     {
         image: weatherapp,
         title: 'Weather Web App',
